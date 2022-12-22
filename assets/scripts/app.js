f89bc34b8782def6153937752b3b473cb838e47b
Ossama kharbaq <ossamakharbaq4@gmail.com>
2022-12-22 15:44:56 +0100

    styling with css and trying to convert that to SASS so

---
+++ assets/scripts/app.js
@@ -0,0 +1,26 @@
+// let homePage = document.getElementById('home');
+// let guidePage = document.getElementById('guide');
+// let quizPage = document.getElementById('quiz');
+// let resultPage = document.getElementById('result');
+
+// let home = document.getElementById('redirect-home');
+// let guide = document.getElementById('redirect-guide');
+// let quiz = document.getElementById('redirect-quiz');
+// let result = document.getElementById('redirect-result');
+
+// console.log(home,guide,quiz,result);
+
+// function currentPage(a,b,c,d){
+//     console.log(a);
+//     // this.classList.add("active");
+//     a.style.display = 'flex';
+//     b.style.display = 'none';
+//     c.style.display = 'none';
+//     d.style.display = 'none';
+// }
+
+// home.addEventListener('click',currentPage(homePage,guidePage,resultPage,quizPage));
+// guide.addEventListener('click',currentPage(guidePage,homePage,resultPage,quizPage));
+// quiz.addEventListener('click',currentPage(quizPage,homePage,guidePage,resultPage));
+// result.addEventListener('click',currentPage(resultPage,homePage,guidePage,quizPage));
+import { questions } from "./questions.js";
