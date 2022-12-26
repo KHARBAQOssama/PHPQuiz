4e2fd6e1d0081b965c915be7cdde491125b1d2fc
Ossama kharbaq <ossamakharbaq4@gmail.com>
2022-12-26 16:57:07 +0100

    finnished all i will see if i will add something else

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,30 @@
-import { questions } from "./questions.js";
-
-let questionsWeHave     = questions;
-let total               = questions.length;
-// console.log(questionsWeHave);
-
-
+const questionsNumber = document.getElementById("questions-number");
+const value = document.getElementById("value");
 // console.log(questionsNumber);
 
 
+questionsNumber.setAttribute('max',questions.length);
+questionsNumber.addEventListener('input',()=>{
+    value.innerText = questionsNumber.value;
+})
+
+
+
 // direction lines
-const to1         = document.getElementById('to1');
-const to2         = document.getElementById('to2');
-const to3         = document.getElementById('to3');
-const gp         = document.getElementById('gp');
-const qp         = document.getElementById('qp');
-const rp         = document.getElementById('rp');
+const to1           = document.getElementById('to1');
+const to2           = document.getElementById('to2');
+const to3           = document.getElementById('to3');
+const to4           = document.getElementById('to4');
+const gp            = document.getElementById('gp');
+const pp            = document.getElementById('pp');
+const qp            = document.getElementById('qp');
+const rp            = document.getElementById('rp');
 
 // buttons 
 const startBtn          = document.getElementById('start-btn');
 const exitGuide         = document.getElementById('exit-guide');
 const takeQuiz          = document.getElementById('take-quiz');
+const start          = document.getElementById('start');
 
 // console.log(startBtn);
 
@@ -28,13 +33,14 @@ const homePage          = document.getElementById('home');
 const guidePage         = document.getElementById('guide');
 const quizPage          = document.getElementById('quiz');
 const resultPage        = document.getElementById('result');
+const paramPage        = document.getElementById('param');
 
 // Result elements 
 
 const feedback          = document.getElementById('feedback'); 
 const questionsTotal    = document.getElementById('questions-total'); 
 const questionsAnswered = document.getElementById('questions-answered'); 
-const playAgain         = document.getElementById('play-again'); 
+// const playAgain         = document.getElementById('play-again'); 
 const exitQuiz          = document.getElementById('exit-quiz'); 
 
 // progress elements
@@ -85,15 +91,35 @@ exitGuide.onclick = () =>{
     
 }
 
+start.onclick=()=>{
+    guidePage.classList.remove("active"); 
+    paramPage.classList.add("active");
+    to2.style.height='100%';
+    setTimeout(()=>{
+        pp.style.backgroundColor = '#5874CC';
+        pp.style.color = 'white';
+    },1000);
+}
+
 exitQuiz.onclick = () =>{
     location.reload();
 }
 
+
+
 var index;
+var total;
+let questionsWeHave =[];
+
+
 takeQuiz.onclick = () =>{
+    for(let i =0;i<questionsNumber.value;i++){
+        questionsWeHave.push(questions[i]);
+    }
+    total = questionsWeHave.length;
     quizPage.classList.add("active"); // show the guide page
-    guidePage.classList.remove("active");
-    to2.style.height='100%';
+    paramPage.classList.remove("active");
+    to3.style.height='100%';
     setTimeout(()=>{
         qp.style.backgroundColor = '#5874CC';
         qp.style.color = 'white';
@@ -102,22 +128,31 @@ takeQuiz.onclick = () =>{
     showQuestion(index);
 }
 
+
+// console.log(total);
+
+
 function showQuestion(index){
     setTimeout(()=>{
         for(let i=0;i<4;i++){
             options[i].classList.remove("disabled");
         }
     },500);
+
+
     question.innerText = questionsWeHave[index]['question'];
     question.setAttribute('data-number',questionsWeHave[index]['answer']);
     for(let i =0 ;i<options.length;i++){
+        
         options[i].innerText = (i+1) +'  -  ' + questionsWeHave[index]['choice'+(i+1)];
     }
+
     questionsWeHave.splice(index,1);
 
     progress.style.width = (((total-questionsWeHave.length)/total)*100) + '%';
-    percentage.innerText = (((total-questionsWeHave.length)/total)*100) + '%';
+    percentage.innerText = Math.floor((((total-questionsWeHave.length)/total)*100)) + '%';
 
+    console.log(total);
     console.log(questionsWeHave.length);
     console.log(question);
 }
@@ -147,7 +182,7 @@ function getAnswer(x){
         }
 
         if(questionsWeHave.length == 0){
-            to3.style.height='100%';
+            to4.style.height='100%';
             setTimeout(()=>{
                 rp.style.backgroundColor = '#5874CC';
                 rp.style.color = 'white';
@@ -167,10 +202,10 @@ function getAnswer(x){
 
 
 function result(){
-    if(correctAnswers == 5){
+    if(correctAnswers == total/2){
         feedback.innerText = "THAT'S NICE";
         questionsAnswered.innerText = correctAnswers;
-    }else if(correctAnswers > 5){
+    }else if(correctAnswers > total/2){
         feedback.innerText = "YOU ARE REALLY SMART";
         questionsAnswered.innerText = correctAnswers;
     }else {
--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -1,4 +1,4 @@
-export var questions = 
+var questions = 
       [
         {
           question: "Que signifie PHP?",
